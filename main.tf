dd3f3b341629797cf2965bc9487118b0a50dd737
Misbaha <misbahauddin1999@gmail.com>
2025-04-13 17:21:32 +0100

    Update

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_s3_bucket" "devsecopsprojectbucket1" {
   bucket = "devsecopsprojectbucket1"
   }
   
+  terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "devsecopsprojectbucket1"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
