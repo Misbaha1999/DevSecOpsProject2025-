06d6e7def582ef298beb04a6c03ffa73fc0e25e8
Misbaha <misbahauddin1999@gmail.com>
2025-04-13 16:33:06 +0100

    Update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform-nov-project23" {
-  bucket = "terraform-nov-project23"
+resource "aws_s3_bucket" "devsecopsprojectbucket1" {
+  bucket = "devsecopsprojectbucket1"
   }
   
